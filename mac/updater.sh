03b496ba67f0328b70869514ca9bceed53f220f4
sn0wstar <cool5tarXV@gmail.com>
2020-09-29 12:11:36 +0800

    Update updater.sh

--- mac/updater.sh
+++ mac/updater.sh
@@ -1,1 +1,10 @@
-#TBA
+cd /
+cd among-us-by-sn0wstar
+echo "downloading... assets"
+
+curl https://github.com/cool5tar/updater-script/raw/among-us/assets/ --output Among%20Us.exe
+curl https://github.com/cool5tar/updater-script/raw/among-us/assets/ --output GameAssembly.dll
+curl https://github.com/cool5tar/updater-script/raw/among-us/assets/ --output UnityCrashHandler32.exe
+curl https://github.com/cool5tar/updater-script/raw/among-us/assets/ --output UnityPlayer.dll
+
+echo "done"
