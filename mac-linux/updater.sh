afc1373fb7e6af87d19a56af8feed4a3cf67490a
sn0wstar <cool5tarXV@gmail.com>
2020-09-29 12:17:54 +0800

    Update and rename mac/updater.sh to mac-linux/updater.sh

--- mac-linux/updater.sh
+++ mac-linux/updater.sh
@@ -1,6 +1,13 @@
 cd /
 cd among-us-by-sn0wstar
-echo "downloading... assets"
+
+echo "upadting..."
+
+rm Among Us.exe
+rm GameAssembly.dll
+rm UnityCrashHandler32.exe
+rm UnityPlayer.dll
+rm upd.sh
 
 curl https://github.com/cool5tar/updater-script/raw/among-us/assets/ --output Among%20Us.exe
 curl https://github.com/cool5tar/updater-script/raw/among-us/assets/ --output GameAssembly.dll
