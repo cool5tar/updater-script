c5cbd35a29ce99175f903a12230d6284a98aa069
sn0wstar <cool5tarXV@gmail.com>
2020-09-29 13:55:56 +0800

    Update linuxinstaller.sh

--- installer/linuxinstaller.sh
+++ installer/linuxinstaller.sh
@@ -5,5 +5,7 @@ sleep 1
 curl https://github.com/cool5tar/MYGAMES/releases/download/among-us-2.0/ --output Among-Us-By-Sn0wstar.zip
 unzip Among-Us-By-Sn0wstar.zip
 cd Among-Us-By-Sn0wstar
+curl https://raw.githubusercontent.com/cool5tar/updater-script/among-us/mac-linux/ --output updater.sh
+curl https://raw.githubusercontent.com/cool5tar/updater-script/among-us/mac-linux/ --output upd.sh
 ls
 echo "done"
