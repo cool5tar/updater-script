d0744aba81b0a1bf54fedb9a20f44ad13c7c4e41
sn0wstar <cool5tarXV@gmail.com>
2020-09-29 12:22:41 +0800

    Update updater.bat

--- win/updater.bat
+++ win/updater.bat
@@ -1,17 +1,18 @@
 cd /
 cd among-us-by-sn0wstar
 
-echo "upadting..."
+echo "updating..."
 
 del Among Us.exe
 del GameAssembly.dll
 del UnityCrashHandler32.exe
 del UnityPlayer.dll
-del upd.sh
+del upd.bat
 
 curl https://github.com/cool5tar/updater-script/raw/among-us/assets/ --output Among%20Us.exe
 curl https://github.com/cool5tar/updater-script/raw/among-us/assets/ --output GameAssembly.dll
 curl https://github.com/cool5tar/updater-script/raw/among-us/assets/ --output UnityCrashHandler32.exe
 curl https://github.com/cool5tar/updater-script/raw/among-us/assets/ --output UnityPlayer.dll
+curl https://raw.githubusercontent.com/cool5tar/updater-script/among-us/win/ --output upd.bat
 
 echo "done"
