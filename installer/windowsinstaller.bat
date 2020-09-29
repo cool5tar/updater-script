e76a4976702a59076e5b7d81630d6bd4a1231044
sn0wstar <cool5tarXV@gmail.com>
2020-09-29 13:48:32 +0800

    Update windowsinstaller.bat

--- installer/windowsinstaller.bat
+++ installer/windowsinstaller.bat
@@ -3,7 +3,7 @@ echo "downloading game..."
 echo "this might take some time..."
 sleep 1
 curl https://github.com/cool5tar/MYGAMES/releases/download/among-us-2.0/ --output Among-Us-By-Sn0wstar.zip
-unzip Among-Us-By-Sn0wstar.zip
+jar xf Among-Us-By-Sn0wstar.zip
 cd Among-Us-By-Sn0wstar
 ls
 echo "done"
